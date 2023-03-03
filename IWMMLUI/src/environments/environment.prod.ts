ec5f5be6b0326a2e3edf23f6ffb960080829d797
sivakumar <sivakumar.kokku@neudesic.com>
2023-03-03 14:57:11 +0530

    updating client id

--- IWMMLUI/src/environments/environment.prod.ts
+++ IWMMLUI/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  clientId: 'b1924ee5-eb10-4a30-bd70-a9837fcf73c7',
+  clientId: '1fbe93a8-0676-4a8c-a825-ba3721cb1391',
   authority: 'https://login.microsoftonline.com/687f51c3-0c5d-4905-84f8-97c683a5b9d1/',
   redirectUrl: 'https://iwmml.azurewebsites.net/',
   apiUrl:'http://127.0.0.1:5000/',
